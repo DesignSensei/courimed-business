cba46418922903532b06ed668f5df7562c00699e
zuke <obilomizuchukwu@gmail.com>
2026-01-09 08:07:11 +0100

    Created Colors, Fonts, and Images constants, and made a Splash screen with animated logo.

---
+++ app/constants/Fonts.ts
@@ -0,0 +1,19 @@
+// constants/Fonts.ts
+
+export const Fonts = {
+  regular: "Nunito_400Regular",
+  medium: "Nunito_500Medium",
+  semiBold: "Nunito_600SemiBold",
+  bold: "Nunito_700Bold",
+  
+  nunitoExtraLight: "Nunito_200ExtraLight",
+  nunitoLight: "Nunito_300Light",
+  nunitoRegular: "Nunito_400Regular",
+  nunitoMedium: "Nunito_500Medium",
+  nunitoSemiBold: "Nunito_600SemiBold",
+  nunitoBold: "Nunito_700Bold",
+  nunitoExtraBold: "Nunito_800ExtraBold",
+  nunitoBlack: "Nunito_900Black",
+} as const;
+
+export default Fonts;
